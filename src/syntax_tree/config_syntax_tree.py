e976f2c20dd2b93cd69dce776c34c7c295780c7a
jbrugger <wwjbrugger@gmail.com>
2025-05-28 15:12:38 +0200

    change default values

--- src/syntax_tree/config_syntax_tree.py
+++ src/syntax_tree/config_syntax_tree.py
@@ -10,12 +10,12 @@ class ConfigSyntaxTree:
                             default=2,
                             help='How many children a node will maximal have')
         parser.add_argument('--max_constants_in_tree', type=int,
-                            default=3,
+                            default=1,
                             help='Maximum number of constants allowed in  equation'
                                  'afterwards equation will be invalid')
         parser.add_argument('--max_depth_of_tree', type=int,
                                     default=10,
                                     help='Maximum depth of generated equations')
         parser.add_argument('--max_num_nodes_in_syntax_tree', type=int,
-                            help='Maximum nodes of generated equations', default=25)
+                            help='Maximum nodes of generated equations', default=15)
         return parser
