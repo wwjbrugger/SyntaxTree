92c2324691b0bff114c9629776703cb77925d1f1
jbrugger <wwjbrugger@gmail.com>
2025-10-28 13:22:01 +0100

    best list per complexity

--- src/syntax_tree/max_list.py
+++ src/syntax_tree/max_list.py
@@ -4,21 +4,41 @@ from bisect import bisect_left
 class MaxList:
     def __init__(self, args):
         self.max_elements_in_list = 10
-        self.max_list_keys = []  # smallest element is left
+        self.max_list_keys = []  # smallest element is right
         self.max_list_state = []
+        self.complexity_dict = {}
         self.args = args
 
-    def add(self, state, key):
-        if len(self.max_list_keys) >= self.max_elements_in_list:
-            if self.max_list_keys[0] >= key:
-                return
+    def add(self, state, key, complexity=True):
+        add(self.max_list_keys,
+            self.max_list_state,
+            state,
+            key,
+            self.max_elements_in_list)
+        if complexity:
+            nom_op = state.syntax_tree.num_inner_nodes()
+            if nom_op not in self.complexity_dict:
+                self.complexity_dict[nom_op] = MaxList(self.args)
+                self.complexity_dict[nom_op].add(
+                    state, key, complexity=False
+                )
             else:
-                self.max_list_keys.pop(0)
-                self.max_list_state.pop(0)
-
-        index = bisect_left(a=self.max_list_keys, x=key)
-        self.max_list_keys.insert(index, key)
-        self.max_list_state.insert(index, state)
+                self.complexity_dict[nom_op].add(
+                    state, key, complexity=False
+                )
 
     def __str__(self):
         return self.max_list_keys.__str__()
+
+
+def add(max_list_keys, max_list_state, state, key, max_elements_in_list):
+    if len(max_list_keys) >= max_elements_in_list:
+        if max_list_keys[0] >= key:
+            return
+        else:
+            max_list_keys.pop(0)
+            max_list_state.pop(0)
+
+    index = bisect_left(a=max_list_keys, x=key)
+    max_list_keys.insert(index, key)
+    max_list_state.insert(index, state)
